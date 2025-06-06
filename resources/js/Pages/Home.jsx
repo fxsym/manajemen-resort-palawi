2ea55a7b583c86a220f02ab581177e7cc67ae66e
fxsym <fsyamsudin24@gmail.com>
2025-06-06 18:30:02 +0700

    First commit, setup project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <div>
+                <p className="text-3xl">Hallo user, ini adalah halaman home dari file baru</p>
+            </div>
+        </>
+    )
+}
