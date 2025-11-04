cace75a6b11f58f8b1618900dba5a65724416417
fxsym <fsyamsudin24@gmail.com>
2025-11-04 10:11:25 +0700

    Add card resorts

--- resources/js/Pages/Resorts.jsx
+++ resources/js/Pages/Resorts.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import MainLayout from "../components/layouts/MainLayout";
+import CardResort from "../components/sections/resort/CardResort";
 
-export default function Resorts({resorts}) {
+export default function Resorts({ resorts }) {
 
     useEffect(() => {
         console.log(resorts)
@@ -11,7 +12,13 @@ export default function Resorts({resorts}) {
         <MainLayout>
             <h1 className="text-3xl font-bold mb-4">Pilih resort</h1>
             <div className="flex flex-col md:flex-row gap-4 w-full justify-around">
-
+                {resorts.length > 0 ? (
+                    resorts.map(({ id, name, image_url }) => (
+                        <CardResort key={id} name={name} image_url={image_url} />
+                    ))
+                ) : (
+                    <p className="text-gray-500">Belum ada resort tersedia</p>
+                )}
             </div>
         </MainLayout>
     )
--- resources/js/components/sections/resort/CardResort.jsx
+++ resources/js/components/sections/resort/CardResort.jsx
@@ -1,7 +1,22 @@
-export default function CardResort () {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function CardResort({ name, image_url }) {
+  return (
+    <div className="relative w-full max-w-sm h-96 rounded-xl overflow-hidden shadow-lg group cursor-pointer">
+      {/* Layer gradient */}
+      <div className="absolute inset-x-0 bottom-0 h-[50%] w-full z-10 bg-gradient-to-t from-black/90 to-transparent opacity-100 group-hover:opacity-10 transition-opacity duration-500"></div>
+
+      {/* Gambar Resort */}
+      <img
+        src={image_url}
+        alt={name}
+        className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+      />
+
+      {/* Teks di bawah */}
+      <div className="absolute inset-x-0 bottom-0 pb-4 pl-6 z-20">
+        <h3 className="text-white text-lg font-semibold drop-shadow-md">
+          {name}
+        </h3>
+      </div>
+    </div>
+  );
 }
